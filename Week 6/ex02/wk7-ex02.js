6b4cfe476771cdf5c0eb8c323e6e395b7d8a8f5e
zhenglong-wu <wu.zhenglong@outlook.com>
2023-11-24 19:48:52 +0000

    Partially completed week 7 seminar exercise

---
+++ Week 6/ex02/wk7-ex02.js
@@ -0,0 +1,11 @@
+function calculate() {
+    var k = 4
+    var l = 15
+    var m = 23.6
+    var n = 7
+    
+    console.log((k+m)/(n))
+    console.log((m+m)/(n))
+    console.log((k*k)/(n))
+    console.log((l-m)/(n+1))
+}
