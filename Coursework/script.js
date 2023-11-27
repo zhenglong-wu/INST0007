5f77235ee7848a66ff26a1eec2bf204738051439
zhenglong-wu <wu.zhenglong@outlook.com>
2023-11-27 22:48:21 +0000

    Almost finished home page of coursework

---
+++ Coursework/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll('.navlink');
+
+    links.forEach(function(link) {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+        else {
+            link.classList.add('inactive')
+        }
+    });
+});
